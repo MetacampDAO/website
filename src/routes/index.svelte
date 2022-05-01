adf0ae2a5bd80c676f524b0a857d7ebc517327bb
jonas002 <lovshains@JChen.localdomain>
2022-05-01 06:13:13 +0200

    website v2

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -22,22 +22,22 @@
 <section class="w-full h-screen font-main">
 	<div class="grid md:h-screen md:grid-cols-2 xs:grid-cols-1 items-center space-x-4 m-4 xs:space-y-6 justify-center">
 		<div 
-		class="flex flex-col mx-auto p-4 justify-center text-6xl text-primary">
+		class="flex flex-col mx-auto p-4 justify-center text-6xl">
 			<div class="text-primary mb-2 text-5xl font-semibold tracking-wider">
 				OpenHaus <span class="text-secondary font-semibold">Singapore</span>
 				
 			</div>
-			<div class="mb-8 text-xl font-semibold text-dark dark:text-light">
+			<div class="mb-8 text-xl font-semibold">
 				Nurturing Web3 Builders & Ventures
 			</div>
 			<div class="flex flex-col justify-end">
-				<a class="flex items-center rounded-md p-4 text-base text-dark dark:text-light font-semibold hover:text-black dark:hover:text-light hover:bg-light dark:hover:bg-dark focus:outline-none focus:ring-1 focus:ring-inset focus:ring-light" href="https://t.me/+_aIlLFNyG49kZWY9" target="_blank" rel="noopener noreferrer">
+				<a class="flex items-center rounded-md p-4 text-base font-semibold hover:text-black dark:hover:text-light hover:bg-light dark:hover:bg-dark focus:outline-none focus:ring-1 focus:ring-inset focus:ring-light" href="https://t.me/+_aIlLFNyG49kZWY9" target="_blank" rel="noopener noreferrer">
 					<div class="justify-center mr-2">
 						<img class="object-center" src="{Telegram}" alt="Telegram">
 					</div>
 					<div><p>Telegram</p></div>
 				</a>
-				<a class="flex items-center rounded-md p-4 text-base text-dark dark:text-light font-semibold hover:text-black dark:hover:text-light hover:bg-light dark:hover:bg-dark focus:outline-none focus:ring-1 focus:ring-inset focus:ring-light" href="https://discord.gg/vHvp6FW3" target="_blank" rel="noopener noreferrer">
+				<a class="flex items-center rounded-md p-4 text-base font-semibold hover:text-black dark:hover:text-light hover:bg-light dark:hover:bg-dark focus:outline-none focus:ring-1 focus:ring-inset focus:ring-light" href="https://discord.gg/vHvp6FW3" target="_blank" rel="noopener noreferrer">
 					<div class="justify-center mr-2">
 						{#if themeValue == "light" }
 							<img src="{blackDiscord}" alt="blackDiscord">
