74fcbefc6aaaeca25fba17f573ac01b6ec8c5d6c
jonas002 <lovshains@JChen.localdomain>
2022-05-01 05:55:17 +0200

    website v2

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -27,11 +27,11 @@
 				OpenHaus <span class="text-secondary font-semibold">Singapore</span>
 				
 			</div>
-			<div class="mb-8 text-dark dark:text-light text-xl font-semibold">
+			<div class="mb-8 text-xl font-semibold">
 				Nurturing Web3 Builders & Ventures
 			</div>
 			<div class="flex flex-col justify-end">
-				<a class="flex items-center rounded-md p-4 text-base  text-dark dark:text-light font-semibold hover:text-black dark:hover:text-light hover:bg-light dark:hover:bg-dark focus:outline-none focus:ring-1 focus:ring-inset focus:ring-light" href="https://t.me/+_aIlLFNyG49kZWY9" target="_blank" rel="noopener noreferrer">
+				<a class="flex items-center rounded-md p-4 text-base text-dark dark:text-light font-semibold hover:text-black dark:hover:text-light hover:bg-light dark:hover:bg-dark focus:outline-none focus:ring-1 focus:ring-inset focus:ring-light" href="https://t.me/+_aIlLFNyG49kZWY9" target="_blank" rel="noopener noreferrer">
 					<div class="justify-center mr-2">
 						<img class="object-center" src="{Telegram}" alt="Telegram">
 					</div>
@@ -49,10 +49,8 @@
 				</a>
 			</div>
 		</div>
-		<div class="flex flex-col space-y-4 mx-auto justify-center rounded-md text-6xl text-primary">
-			<div class="text-primary text-5xl text-center font-semibold">
-				<img class="" src="{OpenHaus}" alt="OpenHaus">
-			</div>
+		<div class="flex flex-col mx-auto p-4 justify-center text-6xl text-primary">
+			<img class="object-fit" src="{OpenHaus}" alt="OpenHaus">
 		</div>
 	</div>
 </section>
